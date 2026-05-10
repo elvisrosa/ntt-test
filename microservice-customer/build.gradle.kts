d61d9b1ba2fe284751f5806c1cbcd489c0a55ddd
Elvis Rosa <montoya103adrian@gmail.com>
2026-05-10 14:33:41 -0500

    Update Spring Boot version to 3.5.14 in build.gradle.kts

--- microservice-customer/build.gradle.kts
+++ microservice-customer/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.4.1"  // ← CAMBIO AQUÍ
+	id("org.springframework.boot") version "3.5.14"
 	id("io.spring.dependency-management") version "1.1.7"
 	id("org.openapi.generator") version "7.2.0"
 }
